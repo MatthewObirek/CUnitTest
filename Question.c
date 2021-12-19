2976b0b5d229860c7d13862bbc5364cbd811c9fe
MatthewObirek <matthewobirek@gmail.com>
2021-12-19 11:33:54 -0800

    added 'answerQuestion' Function

--- Question.c
+++ Question.c
@@ -20,3 +20,32 @@ void printQuestion(Question *Q)
     printf("%s", Q->C);
     printf("%s", Q->D);
 }
+
+int answerQuestion(Question* Q)
+{
+    int input;
+    int correctness = 0;
+    printQuestion(Q);
+    do {
+        printf("Answer(A=1/B=2/C=3/D=4/Hint=5): ");
+        scanf("%d", &input);
+        if (input == Q->answer) 
+        {
+
+            return correctness;//return correct
+        }
+        else if (input == 5)
+        {
+            printf("%s", Q->Hint);
+            printf("Answer(A=1/B=2/C=3/D=4/Hint=5): ");
+            scanf("%d", &input);
+        }
+        else
+        {
+            printf("Incorrect, The correct answer is \"%i\" Try again", Q->answer);
+
+        }
+        correctness++;
+    } while ( input != Q->answer);
+    return correctness;
+}
--- Question.h
+++ Question.h
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
-struct {
+struct
+{
     char Querry[250];
     char A[250];
     char B[250];
@@ -14,4 +15,17 @@ struct {
 
 void createQuestion(Question* Q, char* Querry, char* a, char* b, char* c, char* d, char* Hint, int answer);
 
-void printQuestion(Question *Q);
+void printQuestion(Question* Q);
+
+int answerQuestion(Question* Q);
+
+struct 
+{
+    Question* Start;
+    Question* End;
+} typedef TestList;
+
+void fillTest(char* filename, TestList testlist);
+
+void startTest(TestList testlist);
+
