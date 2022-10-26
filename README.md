5251a9a740fe371f7410276ac6fe8778c96318bb
Matthew Obirek <90856773+MatthewObirek@users.noreply.github.com>
2022-10-26 08:38:47 -0700

    Updated From Crash
    My PC Crashed, This is the non-committed data from that crash.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # CUnitTest
 
-This is a basic program that will quiz the user on any topic. It requires only true or false, or multiple choice questions at the moment. Data that is to be used must be implemented directly into the code. Unstable file reading has been addded. 
-To run the build properly, use `$ ./main nameOfQuiz.txt`
+This is an basic program that will Quiz the user on any topic. It requires only true or false, or multiple choice questions at the moment. Data that is to be used must be implemented directly into the code. Eventually I hope to add functionality that allows files in the same directory to be read.
+
+
+To compile the program `make all` 
+to run the Program: `./Out/Build/main `
--- main.c
+++ main.c
@@ -1,11 +1,135 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 #include "Question.h"
 
+const char* d_name = "Content";
+
 int main(int argc, char* argv[])
 {
-    Question *TestArray;
+
+    DIR *d;
+    //hello
+    struct dirent *dir;
+    d = opendir(d_name);
+    char** d_line = (char**)calloc(10, 50);
+    int i = 0;
+    int l_size = 0;
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL) 
+        {
+            if(!strcmp(dir->d_name, ".") || !strcmp(dir->d_name, ".."))
+                continue;
+            d_line[i] = dir->d_name;
+            printf("%s\n", dir->d_name);
+            i++;
+            l_size = i;
+        }
+        closedir(d);
+    }
+    else
+    {
+        perror("Directory not found, Check directory spelling");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Which files should be loaded?[1,2,...,%d]\n", l_size);
+    //char input[2*l_size];
+    //fgets(input, sizeof(input), stdin);
+    char* f_name = "Content/Example.txt";
+    printf("for now, loading %s...\n", f_name);
+    //! Reading more than 1 file needs to be addressed.
+    /*     
+    char delim[2] = ",";
+    char* ptr = strtok(input, delim);
+    int array[l_size];
+    int n = 0;
+	while(ptr != NULL)
+	{
+        array[n] = ((int)ptr[0]) - 48;
+        n++;
+		ptr = strtok(NULL, delim);
+	}
+    for(int index = 0; index<l_size; index++)
+    {
+        if(array[index] == 0)
+            break;
+        char* f_name[strlen(d_name)+strlen(d_line[array[index]])+1];
+        strcat(f_name, d_line[index]);
+        printf("%s\n", f_name);    
+
+    }*/
+    FILE* file = fopen(f_name, "r");
+    char line[256];
+    int cnt = 0; 
+    Question q_list[50];
+    while (fgets(line, sizeof(line), file)) {
+        /* note that fgets don't strip the terminating \n, checking its
+        presence would allow to handle lines longer that sizeof(line) */
+        char delim[2] = ":";
+        char* ptr = strtok(line, delim);
+        char Querry[256];
+        char Type[256];
+        char ExtraInfo[256];
+        char Hint[256];
+        char Answer[256];
+        ///printf("%s\n", ptr);
+        if (strcmp(ptr, "!Question") == 0)
+        {
+            ptr+=11;
+            ///printf("%s\n", ptr);            
+            strcpy(Querry, ptr);
+        }
+        else if(strcmp(ptr, "!QType") == 0)
+        {   
+            ptr+=8;
+            ///printf("%s\n", ptr);
+            strcpy(Type, ptr);
+        }
+        else if(strcmp(ptr, "!ExtraInfo") == 0)
+        {
+            ptr+=12;
+            ///printf("%s\n", ptr);
+            strcpy(ExtraInfo, ptr);
+        }
+        else if(strcmp(ptr, "!Hint") == 0)
+        {
+            ptr+=7;
+            ///printf("%s\n", ptr);
+            strcpy(Hint, ptr);
+        }
+        else if(strcmp(ptr, "!Answer") == 0)
+        {
+            ptr+=9;
+            ///printf("%s\n", ptr);
+            strcpy(Answer, ptr);
+        }        
+        else if(strcmp(line, "!EndQuestion") == 0)
+        {
+
+            if(cnt>49)
+                break;
+            createQuestion(&q_list[cnt], Querry, Type, ExtraInfo, Hint, Answer);
+            printf("count: %d\n",cnt);
+            cnt++; 
+        }
+        
+    }
+
+
+    for(int x = 0; x < cnt; x++)
+    {
+        printf("\n\ncount: %d\n", x);
+        printQuestion(&q_list[x]);
+    }
+    fclose(file);
+    return 0;
+}
+
+
+    /*Question *TestArray;
     printf("%d\n", argc);
     if(argc > 1)
     {
@@ -15,7 +139,7 @@ int main(int argc, char* argv[])
         int cnt = 0; 
         while (fgets(line[0], sizeof(line), file)) {
             /* note that fgets don't strip the terminating \n, checking its
-            presence would allow to handle lines longer that sizeof(line) */
+            presence would allow to handle lines longer that sizeof(line) 
             cnt++;
          
         }
@@ -28,7 +152,7 @@ int main(int argc, char* argv[])
         int j = 0;
         while (fgets(line[0], sizeof(line), file)) {
             /* note that fgets don't strip the terminating \n, checking its
-            presence would allow to handle lines longer that sizeof(line) */
+            presence would allow to handle lines longer that sizeof(line) 
             for(int i = 1; i < 7; i++)
                 fgets(line[i], sizeof(line[i]), file);
             int x;
@@ -39,8 +163,7 @@ int main(int argc, char* argv[])
          
         }
         /* may check feof here to make a difference between eof and io failure -- network
-        timeout for instance */
-
+        timeout for instance *
         fclose(file);
     }
     else
@@ -70,5 +193,4 @@ int main(int argc, char* argv[])
     else
     {
         printf("Bad Job\n");
-    }
-}
+    }*/
